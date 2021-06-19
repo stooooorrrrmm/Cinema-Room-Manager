3e33861ee5ef5df0558b3f61dace370c683e2c72
hahaskazka <hahaskazka@gmail.com>
2021-06-19 12:22:14 +0300

    Added calculation of the ticket price and display of the seat in the hall

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,15 @@ public class Main {
     static void getInfo() {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the number of rows:"); System.out.print("> ");
-        int numberOfRows = in.nextInt(); System.out.println();
+        System.out.println("Enter the number of rows:");
+        System.out.print("> ");
+        int numberOfRows = in.nextInt();
+        System.out.println();
 
-        System.out.println("Enter the number of seats in each row: "); System.out.print("> ");
-        int numberOfSeats = in.nextInt(); System.out.println();
+        System.out.println("Enter the number of seats in each row: ");
+        System.out.print("> ");
+        int numberOfSeats = in.nextInt();
+        System.out.println();
 
         String[][] field = new String[numberOfRows][numberOfSeats];
 
@@ -21,16 +25,18 @@ public class Main {
             }
         }
 
-        printField(field,numberOfRows,numberOfSeats);
+        printField(field, numberOfRows, numberOfSeats);
+        getTickets(field, numberOfRows, numberOfSeats);
     }
 
     static void printField(String[][] field, int numberOfRows, int numberOfSeats) {
-        System.out.println("Cinema:"); System.out.print("  ");
+        System.out.println("Cinema:");
+        System.out.print("  ");
 
         for (int i = 0; i <= numberOfRows; i++) {
-            System.out.print((i+1) + " ");
+            System.out.print((i + 1) + " ");
         }
-        System.out.println("");
+        System.out.println();
 
         for (int i = 0; i < numberOfRows; i++) {
             System.out.print((i + 1) + " ");
@@ -41,37 +47,56 @@ public class Main {
         }
     }
 
-    static void sold() {
-        Scanner in = new Scanner(System.in);
+    static void sold(String[][] field, int numberOfRows, int numberOfSeats, int rowNumber) {
 
-        System.out.println("Enter the number of rows: "); System.out.print("> ");
-        int numberOfRows = in.nextInt(); System.out.println();
-
-        System.out.println("Enter the number of seats in each row: "); System.out.print("> ");
-        int numberOfSeats = in.nextInt(); System.out.println();
-
-        int sold = 0; int front; int back;
+        int sold = 10;
 
         if (numberOfRows * numberOfSeats < 60) {
-            sold = (numberOfRows * numberOfSeats) * 10;
-        }
-        else {
+            sold = 10;
+        } else {
             if (numberOfRows % 2 == 0) {
-                front = ((numberOfRows / 2) * numberOfSeats) * 10;
-                back = ((numberOfRows / 2) * numberOfSeats) * 8;
-                sold = front + back;
-            }
-            else {
+                if (rowNumber < numberOfRows / 2) {
+                    sold = 10;
+                } else {
+                    if (rowNumber > numberOfRows / 2) {
+                        sold = 8;
+                    }
+                }
+            } else {
                 if (numberOfRows % 2 != 0) {
-                    front = (((numberOfRows - 1) / 2) * numberOfSeats) * 10;
-                    back = (((numberOfRows + 1) / 2) * numberOfSeats) * 8;
-                    sold = front + back;
+                    if (rowNumber < (numberOfRows - 1) / 2) {
+                        sold = 10;
+                    } else {
+                        if (rowNumber > (numberOfRows - 1) / 2) {
+                            sold = 8;
+                        }
+                    }
                 }
             }
         }
 
-        System.out.println("Total income:");
+        System.out.print("Ticket price: ");
         System.out.println("$" + sold);
+        System.out.println();
+
+        printField(field, numberOfRows, numberOfSeats);
+    }
+
+    static void getTickets(String[][] field, int numberOfRows, int numberOfSeats) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\nEnter a row number:");
+        System.out.print("> ");
+        int rowNumber = in.nextInt();
+        System.out.println();
+
+        System.out.println("Enter a seat number in that row:");
+        System.out.print("> ");
+        int seatNumber = in.nextInt();
+        System.out.println();
+
+        field[rowNumber - 1][seatNumber - 1] = "B";
+        sold(field, numberOfRows, numberOfSeats, rowNumber);
     }
 
     public static void main(String[] args) {
@@ -80,13 +105,3 @@ public class Main {
         }
     }
 }
-
-//Cinema:
-//  1 2 3 4 5 6 7 8
-//1 S S S S S S S S
-//2 S S S S S S S S
-//3 S S S S S S S S
-//4 S S S S S S S S
-//5 S S S S S S S S
-//6 S S S S S S S S
-//7 S S S S S S S S
