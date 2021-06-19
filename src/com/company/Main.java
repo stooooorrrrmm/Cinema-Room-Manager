0e4dd380201439d548c8d8ec3c1ae4d28b2193a4
hahaskazka <hahaskazka@gmail.com>
2021-06-19 11:02:55 +0300

    Add calculating the profit from the sold tickets depending on the number of seats

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,48 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    static void printField(String[][] grid) {
+    static void printField(String[][] field) {
         System.out.println("Cinema:");
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[r].length; c++)
-                System.out.print(grid[r][c] + " ");
+        for (String[] strings : field) {
+            for (String string : strings) System.out.print(string + " ");
             System.out.println();
         }
+    }
+
+    static void sold() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: "); System.out.print("> ");
+        int numberOfRows = in.nextInt(); System.out.println();
+
+        System.out.println("Enter the number of seats in each row: "); System.out.print("> ");
+        int numberOfSeats = in.nextInt(); System.out.println();
+
+        int sold = 0; int front; int back;
+
+        if (numberOfRows * numberOfSeats < 60) {
+            sold = (numberOfRows * numberOfSeats) * 10;
+        }
+        else {
+            if (numberOfRows % 2 == 0) {
+                front = ((numberOfRows / 2) * numberOfSeats) * 10;
+                back = ((numberOfRows / 2) * numberOfSeats) * 8;
+                sold = front + back;
+            }
+            else {
+                if (numberOfRows % 2 != 0) {
+                    front = (((numberOfRows - 1) / 2) * numberOfSeats) * 10;
+                    back = (((numberOfRows + 1) / 2) * numberOfSeats) * 8;
+                    sold = front + back;
+                }
+            }
+        }
+
+        System.out.println("Total income:");
+        System.out.println("$" + sold);
     }
 
 
@@ -24,5 +58,6 @@ public class Main {
                 { "7", "S", "S", "S", "S", "S", "S", "S", "S" }
         };
         printField(field);
+        sold();
     }
 }
